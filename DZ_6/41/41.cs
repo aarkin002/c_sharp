6908c13c6867b212d7ecd78ea6e185a90d544d1e
Yarkin Alexey <aarkin002@gmail.com>
2023-07-04 13:56:32 +0300

    ДЗ_6 - Готово

---
+++ DZ_6/41/41.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите кол-во чисел M: ");
+int m = int.Parse(Console.ReadLine());
+int cnt = 0;
+for(int i=0; i < m; i++){
+    Console.WriteLine("Введите {0} число: ", i+1);
+    int tmp = int.Parse(Console.ReadLine());
+    if(tmp>0){
+        cnt++;
+    }
+}
+Console.WriteLine("Cколько чисел больше 0:  {0}", cnt);
---
+++ DZ_6/43/43.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+double[] Dot(double b1,double k1, double b2, double k2){
+    double x = (b2 - b1)/(k1-k2);
+    double y = k1*x + b1;
+    Console.WriteLine(x);
+    Console.WriteLine(y);
+    double[] dotArr = new double[2]{x, y};
+    return dotArr;
+}
+
+Console.WriteLine("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+
+double[] dot = Dot(b1,k1,b2,k2);
+
+Console.WriteLine("[{0}]", string.Join(", ", dot));
