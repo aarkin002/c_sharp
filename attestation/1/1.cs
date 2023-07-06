e43bb083cafa9aa07b1991a78bd1df127f7eaa81
Yarkin Alexey <aarkin002@gmail.com>
2023-07-06 13:57:56 +0300

    Аттестация - Готово не полностью

---
+++ attestation/1/1.cs
@@ -0,0 +1,11 @@
+﻿void wtf(int m, int n){
+    for(int i = m; i <=n; i++) {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+wtf(m, n);
---
+++ attestation/2/2.cs
@@ -0,0 +1,13 @@
+﻿int wtf(int m, int n){
+    int sum = 0;
+    for(int i = m; i <=n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(wtf(m, n));
